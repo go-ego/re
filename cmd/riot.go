d434255b6d6b4a7fab265a02557acfbe95bfec96
vcaesar <vzvway@gmail.com>
2017-08-19 22:44:37 +0800

    Update and remove data

--- cmd/riot.go
+++ cmd/riot.go
@@ -33,7 +33,7 @@ var cmdRiot = &Command{
 func createRiot(cmd *Command, args []string) int {
 	gopath := GetGOPATHs()
 	fmt.Println(gopath)
-	githubsrc := gopath[0] + "/src/github.com/go-ego/re/riot/"
+	githubsrc := gopath[0] + "/src/github.com/go-ego/riot/data"
 	if runtime.GOOS == "windows" {
 		githubsrc = strings.Replace(githubsrc, "/", "\\", -1)
 	}
